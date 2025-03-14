90e4871bde6aa01cc2c41f0dcd7da9b3cd1f6922
sun123zxy <304244450@qq.com>
2025-03-15 02:46:18 +0800

    done

--- _assets/callthm.lua
+++ _assets/callthm.lua
@@ -1,7 +1,11 @@
+local function starts_with(start, str)
+  return str:sub(1, #start) == start
+end
+
 local function callout_tbl(type, appearance, collapse, icon)
   local appearance = appearance or "minimal"
-  local collapse = collapse or nil
-  local icon = icon or nil
+  local collapse = collapse or ""
+  local icon = icon or ""
   return {
     type = type,
     appearance = appearance,
@@ -11,51 +15,55 @@ local function callout_tbl(type, appearance, collapse, icon)
 end
 
 local thmmap = {
-  thm = callout_tbl("note"),
-  lem = callout_tbl("note"),
-  cor = callout_tbl("note"),
-  prp = callout_tbl("note"),
-  cnj = callout_tbl("note"),
-  def = callout_tbl("note"),
-  exm = callout_tbl("tip"),
-  exr = callout_tbl("tip"),
-  Proof = callout_tbl("note", "default", true),
-  Solution = callout_tbl("note", "default", true),
-  Remark = callout_tbl("tip", "default", true)
+  thm = {"Theorem", callout_tbl("note")},
+  lem = {"Lemma", callout_tbl("note")},
+  cor = {"Corollary", callout_tbl("note")},
+  prp = {"Proposition", callout_tbl("note")},
+  cnj = {"Conjecture", callout_tbl("note")},
+  def = {"Definition", callout_tbl("note")},
+  exm = {"Example", callout_tbl("tip")},
+  exr = {"Exercise", callout_tbl("tip")},
+  proof = {"Proof", callout_tbl("note", "default", "true")},
+  solution = {"Solution", callout_tbl("note", "default", "true")},
+  remark = {"Remark", callout_tbl("tip", "default", "false")}
 }
 
-local function filter(el)
-    local refType = function (id)
-      local match = string.match(id, "^(%a+)%-")
-      if match then
-        return pandoc.text.lower(match)
-      else
-        return nil
-      end
-    end
-    local type = el.type or refType(el.identifier)
-    if thmmap[type] then
-      local name = pandoc.List({type})
-      if el.name then
-        name:insert(" (")
-        name:extend(pandoc.utils.blocks_to_inlines(pandoc.Blocks(el.name)))
-        name:insert(")")
-      end
-    
-      local tbl = thmmap[type]
-      tbl.title = name
-      tbl.content = el.div
-      local cl = quarto.Callout(tbl)
-      return cl
-    end
+local function fetch_header(el)
+  if el.content == nil then
+    return nil
+  end
+  if el.content[1].t == "Header" and el.content[1].level == 2 then
+    return pandoc.utils.stringify(el.content[1])
+  else 
+    return nil
+  end
 end
 
-
 if quarto.doc.is_format("html") then
-  function Theorem(el)
-    return filter(el)
-  end
-  function Proof(el)
-    return filter(el)
+  function Div(el)
+    for index, value in pairs(thmmap) do
+      local fullname = value[1]
+      local tbl = value[2]
+      if pandoc.List.find(el.attr.classes, index) or starts_with(index, el.attr.identifier) then
+        
+        if tbl == nil then
+          return el
+        end
+
+        local title = nil
+        if tbl.appearance ~= "minimal" then
+          title = fetch_header(el)
+          if title ~= nil then
+            title = fullname .. " (" .. title .. ")"
+          else
+            title = fullname
+          end
+        end
+        local classes = {appearance = tbl.appearance, collapse = tbl.collapse, icon = tbl.icon, title = title}
+        return pandoc.Div(el, pandoc.Attr("", {"callout-"..tbl.type}, classes))
+      end
+    end
+
+    return el
   end
 end

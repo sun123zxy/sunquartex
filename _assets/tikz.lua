2f2d02ecd136ca1aa7d81890f24f25f33d375ad1
sun123zxy <304244450@qq.com>
2025-03-14 15:55:19 +0800

    tikz code improvements

--- _assets/tikz.lua
+++ _assets/tikz.lua
@@ -7,9 +7,7 @@ local system = require 'pandoc.system'
 
 local function read_file(file)
   local f = io.open(file, 'r')
-  if f == nil then
-    return nil
-  end
+  if not f then return nil end
   local content = f:read('a')
   f:close()
   return content
@@ -17,62 +15,58 @@ end
 
 local tikz_doc_template = read_file(os.getenv("QUARTO_PROJECT_DIR")..'/_assets/suntemp-tikz.tex')
 
+local function execute_command(command, log_file)
+  print("executing: " .. command)
+  local status = os.execute(command .. ' > ' .. log_file)
+  if status ~= true then
+    local log = read_file(log_file)
+    return false, log
+  end
+  return true
+end
+
 local function tikz2image(src, filetype)
-  local str = ""
-  system.with_temporary_directory('tikz2image', function (tmpdir)
-    system.with_working_directory(tmpdir, function()
+  return system.with_temporary_directory('tikz2image', function (tmpdir)
+    return system.with_working_directory(tmpdir, function()
+      
       local f = io.open('mytikz.tex', 'w')
-      if f == nil then
-        str = "failed to open extracted TeX file"
-        error(str)
-        return nil
+      if not f then error("failed to open extracted TeX file") end
+      
+      if not tikz_doc_template then
+        error("Cannot find the template for tikz.")
       end
       f:write(tikz_doc_template:format(src))
       f:close()
-      -- if use dvi/xdv output
-      -- os.execute('xelatex -no-pdf mytikz.tex')
-      -- os.execute('dvisvgm mytikz.xdv --zoom=1.5 --font-format=woff2 --no-styles')
-      os.execute('xelatex mytikz.tex')
-      os.execute('dvisvgm mytikz.pdf --pdf --zoom=1.5 --font-format=woff2 --no-styles')
-      local g = io.open('mytikz.svg', 'r')
-      if g == nil then
-        str = "failed to open converted SVG file"
-        error(str)
-        return nil
+      
+      result, log = execute_command('xelatex mytikz.tex', 'xelatex_output.log')
+      if not result then
+        print(log)
+        error("failed to compile TeX file to PDF. Logs printed above.")
       end
-      str = g:read("a")
-      g:close()
-      return nil
-    end)
-    return nil
-  end)
-  return str
-end
 
-local function file_exists(name)
-  local f = io.open(name, 'r')
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
-  end
+      result, log = execute_command('dvisvgm mytikz.pdf --pdf --zoom=1.5 --font-format=woff2 --no-styles', 'dvisvgm_output.log')
+      if not result then
+        print(log)
+        error("failed to convert PDF file to SVG. Logs printed above.")
+      end
+      
+      str = read_file('mytikz.svg')
+      if not str then
+        error("failed to open converted SVG file")
+      end
+      return str
+    end)
+  end)
 end
 
 function CodeBlock(el)
   local type = el.classes[1]
-  -- quarto.log.output(type)
   if type == "{tikz}" then
     if quarto.doc.is_format("pdf") or quarto.doc.is_format("beamer") then
       return pandoc.RawInline("latex", el.text)
     elseif quarto.doc.is_format("html") then
       local filetype = 'svg'
-      -- local fbasename = pandoc.sha1(el.text) .. '.' .. filetype
-      -- local fname = fbasename
-      local svg = ""
-      -- if not file_exists(fname) then
-      svg = tikz2image(el.text, filetype)
-      -- end
+      local svg = tikz2image(el.text, filetype)
       return pandoc.Div(pandoc.RawInline("html", svg), pandoc.Attr("", {"tikz"}))
     else
       return el

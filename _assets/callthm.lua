a8812f93993bd608746959729aaa5ca245882970
sun123zxy <304244450@qq.com>
2025-03-15 01:18:34 +0800

    temp: post-ast, need to handle crossref, need to create thm/pf

--- _assets/callthm.lua
+++ _assets/callthm.lua
@@ -1,11 +1,7 @@
-local function starts_with(start, str)
-  return str:sub(1, #start) == start
-end
-
-local function callout_dict(type, appearance, collapse, icon)
+local function callout_tbl(type, appearance, collapse, icon)
   local appearance = appearance or "minimal"
-  local collapse = collapse or ""
-  local icon = icon or ""
+  local collapse = collapse or nil
+  local icon = icon or nil
   return {
     type = type,
     appearance = appearance,
@@ -15,65 +11,51 @@ local function callout_dict(type, appearance, collapse, icon)
 end
 
 local thmmap = {
-  thm = callout_dict("note"),
-  lem = callout_dict("note"),
-  cor = callout_dict("note"),
-  prp = callout_dict("note"),
-  cnj = callout_dict("note"),
-  def = callout_dict("note"),
-  exm = callout_dict("tip"),
-  exr = callout_dict("tip")
+  thm = callout_tbl("note"),
+  lem = callout_tbl("note"),
+  cor = callout_tbl("note"),
+  prp = callout_tbl("note"),
+  cnj = callout_tbl("note"),
+  def = callout_tbl("note"),
+  exm = callout_tbl("tip"),
+  exr = callout_tbl("tip"),
+  Proof = callout_tbl("note", "default", true),
+  Solution = callout_tbl("note", "default", true),
+  Remark = callout_tbl("tip", "default", true)
 }
 
-local pfmap = {
-  proof = callout_dict("note", "default", "true"),
-  solution = callout_dict("note", "default", "true"),
-  remark = callout_dict("tip", "default", "false")
-}
-
-local function fetch_header(el)
-  if el.content == nil then
-    return nil
-  end
-  if el.content[1].t == "Header" and el.content[1].level == 2 then
-    return pandoc.utils.stringify(el.content[1])
-  else 
-    return nil
-  end
+local function filter(el)
+    local refType = function (id)
+      local match = string.match(id, "^(%a+)%-")
+      if match then
+        return pandoc.text.lower(match)
+      else
+        return nil
+      end
+    end
+    local type = el.type or refType(el.identifier)
+    if thmmap[type] then
+      local name = pandoc.List({type})
+      if el.name then
+        name:insert(" (")
+        name:extend(pandoc.utils.blocks_to_inlines(pandoc.Blocks(el.name)))
+        name:insert(")")
+      end
+    
+      local tbl = thmmap[type]
+      tbl.title = name
+      tbl.content = el.div
+      local cl = quarto.Callout(tbl)
+      return cl
+    end
 end
 
+
 if quarto.doc.is_format("html") then
-  function Div(el)
-    for index, value in pairs(thmmap) do
-      if starts_with(index, el.attr.identifier) then
-        if value == nil then
-          return el
-        end
-
-        return pandoc.Div(el, pandoc.Attr("", {"callout-"..value.type}, {appearance = value.appearance, collapse = value.collapse, icon = value.icon}))
-      end
-    end
-    
-    for index, value in pairs(pfmap) do
-      if pandoc.List.find(el.attr.classes, index) then
-        
-        if value == nil then
-          return el
-        end
-
-        local title = fetch_header(el)
-        
-        if title ~= nil then
-          title = index .. ": " .. title
-        elseif value.collapse ~= "" then
-          title = index
-        end
-
-        local classes = {appearance = value.appearance, collapse = value.collapse, icon = value.icon, title = title}
-        return pandoc.Div(el, pandoc.Attr("", {"callout-"..value.type}, classes))
-      end
-    end
-
-    return el
+  function Theorem(el)
+    return filter(el)
+  end
+  function Proof(el)
+    return filter(el)
   end
 end
